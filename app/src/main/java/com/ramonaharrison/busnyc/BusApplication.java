83129bc9af66c79c33b2e1527c0185709070da44
ramonaharrison <ramona.e.harrison@gmail.com>
2016-02-18 19:22:03 -0500

    new android project

---
+++ app/src/main/java/com/ramonaharrison/busnyc/BusApplication.java
@@ -0,0 +1,11 @@
+package com.ramonaharrison.busnyc;
+import android.app.Application;
+
+/**
+ * Created by Ramona Harrison
+ * on 2/18/16.
+ */
+
+public class BusApplication extends Application
+{
+}
